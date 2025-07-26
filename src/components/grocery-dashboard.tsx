ca4f4cc28d00b83210465f8ab631e97b3d1505e0
Manvendra Upadhyay <manvendraupadhyay.cs@gmail.com>
2025-07-26 10:56:20 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/grocery-dashboard.tsx
+++ src/components/grocery-dashboard.tsx
@@ -2,6 +2,7 @@
 "use client";
 
 import * as React from "react";
+import Link from "next/link";
 import { MoreHorizontal, PlusCircle, Upload, Loader2, LogOut } from "lucide-react";
 import {
   Card,
